4e004174cf5492d0743bb0cb5cd2d03e8d74d734
beth deutmeyer <bethdeutmeyer@gmail.com>
2023-12-04 11:54:28 -0600

    backend for /me is working, modified models and seeds to accommodate graphql's needs

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,12 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css'
 
 import App from './App.jsx'
-import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import Profile from './pages/Profile';
 import Error from './pages/Error';
 import Landing from './pages/Landing';
+
 
 const router = createBrowserRouter([
   {
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,6 +4,40 @@ const typeDefs = `
     name: String
     email: String
     password: String
+    electricConsumption: [ElectricConsumption]
+    naturalGasConsumption: [NaturalGasConsumption]
+    gasolineConsumption: [GasolineConsumption]
+    snapshots: [Snapshot]
+  }
+
+  type ElectricConsumption {
+    userId: ID
+    kWh: Int
+    billDate: String
+    comment: String
+  }
+
+  type NaturalGasConsumption {
+    userId: ID
+    therms: Int
+    billDate: String
+    comment: String
+  }
+
+  type GasolineConsumption {
+    userId: ID
+    gallons: Int
+    purchaseDate: String
+    comment: String
+  }
+
+  type Snapshot {
+    userId: ID
+    startDate: String
+    endDate: String
+    electricFootprint: Int
+    naturalGasFootprint: Int
+    gasolineFootprint: Int
   }
 
   type Auth {
