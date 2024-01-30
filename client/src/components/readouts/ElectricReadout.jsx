5544f53eee908015413033c4eca5dbf5873046e4
beth deutmeyer <bethdeutmeyer@gmail.com>
2024-01-30 16:19:12 -0600

    calculations now successfully changing dynamically based on db percentages pulled from backend instead of hardcoded in file. so any added companies just need to be added to seeder file and db seeded

--- client/src/components/readouts/ElectricReadout.jsx
+++ client/src/components/readouts/ElectricReadout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import formatDate from '../../utils/formatDate'
 
-const ElectricReadout = ({ electricCompany, kwh, billDate, carbonOutput, individualFootprints }) => {
+const ElectricReadout = ({ electricCompany, kwh, billDate, carbonSum, calcArray }) => {
 
   const formattedDate = formatDate(billDate)
 
@@ -11,16 +11,22 @@ const ElectricReadout = ({ electricCompany, kwh, billDate, carbonOutput, individ
       <p id='natGasText'>Electric Company: {electricCompany}</p>
       <p id='natGasText'>kWh: {kwh ? kwh : ''}</p>
       <p id='natGasText'>Electric Bill Date: {billDate ? formattedDate : ''}</p>
-      
+
       {/* Display breakdown of carbon footprint for each energy source */}
       <p id='natGasText'>Carbon Footprint Breakdown:</p>
+      <div>
+        {calcArray.map((obj, index) => {
+          const key = Object.keys(obj)[0];
+          const value = Object.values(obj)[0];
 
-      {  
-        Object.entries(individualFootprints).map(item => (
-          <p id='natGasText' key={item[0]}>{item[0]} = {kwh ? item[1] : ''}</p>
-        ))
-      }
-      <p id='natGasText'>Total Carbon Footprint: {carbonOutput ? `${carbonOutput} pounds of CO₂` : '' } </p>
+          return (
+            <div key={index}>
+              <p>{key}{value}</p>
+            </div>
+          );
+        })}
+      </div>
+      <p id='natGasText'>Total Carbon Footprint: {carbonSum ? `${carbonSum.toFixed(0)} pounds of CO₂` : ''} </p>
       {/* Display other calculation results or further user interaction */}
     </div>
   );
